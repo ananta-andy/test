c684a5e3310a5e52eecdab5bdc278cad3c37a3a4
ananta-andy <anantasahu1998@gmail.com>
2021-09-09 07:20:32 +0530

    one line added

--- mango.py
+++ mango.py
@@ -1,1 +1,12 @@
-print("hello")
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = str(age)
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+    print("hello my age is " + abc.age)
+p1 = Person("John", 36)
+p1.myfunc()
+
